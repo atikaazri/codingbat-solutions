5d1e35fb1222a720689ffc201d9d38839e022528
atikaazri <92602068+atikaazri@users.noreply.github.com>
2025-07-27 14:30:18 +0400

    Corrected hasBad.java

--- string1/hasBad.java
+++ string1/hasBad.java
@@ -1,7 +1,9 @@
 public boolean hasBad(String str) {
   if(str.length()<3)return false;
-  if(str.substring(0,3).equals("bad") || str.substring(1,4).equals("bad")){
+  if(str.substring(0,3).equals("bad")){
     return true;
   }
+  if(str.length()>3 && str.substring(1,4).equals("bad")) return true;
   return false;
 }
+
