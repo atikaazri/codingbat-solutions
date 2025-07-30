345eb11d6774021654014d58ae2224e20d99608e
Atika Al Azri <s138631@student..squ.edu.om>
2025-07-30 13:46:28 +0400

    Add solution for array3-linearIn

---
+++ array3/linearIn.java
@@ -0,0 +1,14 @@
+public boolean linearIn(int[] outer, int[] inner) {
+  int i = 0;
+  int j =0;
+  while(i<outer.length && j<inner.length){
+    if(outer[i] < inner[j]) i++;
+    else if(outer[i] == inner[j]) {
+      i++; j++;
+      
+    }
+    else return false;
+  }
+  if(j<inner.length) return false;
+  return true;
+}
