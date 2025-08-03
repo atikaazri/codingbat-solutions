3ff89b9bdeb34a29e51d9130344829ffc2a1dfe7
Atika Al Azri <s138631@student..squ.edu.om>
2025-08-04 00:35:33 +0400

    Add solution for recursion1-strDist

---
+++ recursion1/strDist.java
@@ -0,0 +1,7 @@
+public int strDist(String str, String sub) {
+  if (str.length() < sub.length()) return 0;
+  if (str.startsWith(sub) && str.endsWith(sub)) return str.length();
+  else if (!str.startsWith(sub)) return strDist(str.substring(1), sub);
+  else return strDist(str.substring(0, str.length() - 1), sub);
+
+}
